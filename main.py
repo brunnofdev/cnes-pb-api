566b6a8713543e3af93262f8cd05be5e90119e4a
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2025-11-18 16:09:33 -0300

    Correção de execução do main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -11,3 +11,6 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
