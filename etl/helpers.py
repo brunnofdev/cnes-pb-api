5cd2e8c5bea72eb2fdbaf78a2a6203a7a82d8f72
ArthurHDEV <arthurhap.pro@gmail.com>
2025-11-24 09:33:36 -0300

    Adição de função para padronizar sinônimos e abreviações no DataFrame

--- etl/helpers.py
+++ etl/helpers.py
@@ -35,3 +35,37 @@ def remover_acentos(texto: str) -> str:
         return str(texto)
     return ''.join(c for c in unicodedata.normalize('NFD', texto)
                   if unicodedata.category(c) != 'Mn').upper().strip()
+
+# Padronização de sinônimos e abreviações
+def tratar_sinonimos(df: pd.DataFrame) -> pd.DataFrame:
+
+    # Abreviação de termos comuns em endereços
+    if 'no_logradouro' in df.columns:
+        # Dicionário de Regex: \b garante palavra exata
+        regras = {
+            r'\bAVENIDA\b': 'AV',
+            r'\bRUA\b': 'R',
+            r'\bDOUTOR\b': 'DR',
+            r'\bPRESIDENTE\b': 'PRES',
+            r'\bCORONEL\b': 'CEL',
+            r'\bPRACA\b': 'PC',
+            r'\bPROFESSOR\b': 'PROF',
+            r'\bENGENHEIRO\b': 'ENG'
+        }
+        
+        for padrao, substituto in regras.items():
+            df['no_logradouro'] = df['no_logradouro'].str.replace(padrao, substituto, regex=True)
+
+    # Padronização dos turnos de atendimento
+    if 'ds_turno_atendimento' in df.columns:
+        mapa_turnos = {
+            'ATENDIMENTOS NOS TURNOS DA MANHA E A TARDE': 'DIURNO',
+            'ATENDIMENTO NOS TURNOS DA MANHA, TARDE E NOITE': 'INTEGRAL',
+            'ATENDIMENTO CONTINUO DE 24 HORAS/DIA (PLANTAO:INCLUI SABADOS, DOMINGOS E FERIADOS)': 'PLANTAO 24H',
+            'ATENDIMENTO SOMENTE PELA MANHA': 'MANHA',
+            'ATENDIMENTO SOMENTE A TARDE': 'TARDE',
+            'ATENDIMENTO SOMENTE A NOITE': 'NOITE'
+        }
+        df['ds_turno_atendimento'] = df['ds_turno_atendimento'].replace(mapa_turnos)
+
+    return df
